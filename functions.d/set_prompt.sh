251a58aca1fd8026ca0a381988eb67cf94d0c320
Franz Knipp <franz.knipp@fh-burgenland.at>
2021-10-02 11:50:15 +0200

    Add shellcheck

--- functions.d/set_prompt.sh
+++ functions.d/set_prompt.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
 shopt -s promptvars
 
+check() {
+  echo::error "Add check function to your bashrc file."
+}
+
+# shellcheck disable=SC2154
 PS1="\$(check)\[\n\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
--- functions.sh
+++ functions.sh
@@ -1,3 +1,6 @@
-for i in ../functions.d/* ; do
+#!/bin/bash
+
+for i in ../functions.d/*; do
+  # shellcheck disable=SC1090
   . "$i"
 done
