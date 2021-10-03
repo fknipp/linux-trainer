2dc427024327c9b58d280c2850d87e47df27d92f
Franz Knipp <franz.knipp@fh-burgenland.at>
2021-10-03 12:38:21 +0200

    Add bold text style

--- 0.0_Tests/config/02 test last command.bashrc
+++ 0.0_Tests/config/02 test last command.bashrc
@@ -4,8 +4,6 @@
 . ../functions.sh
 
 check() {
-  cat <<EOF
-The last command was $1. 
-Call exercise::set_success to mark the success of the exercise.
-EOF
+  echo -e "The last command was ${C_BOLD}$1${C_NORMAL}."
+  echo -e "Call ${C_BOLD}exercise::set_success${C_NORMAL} to mark the success of the exercise."
 }
--- functions.d/echo.sh
+++ functions.d/echo.sh
@@ -1,8 +1,11 @@
-C_ERROR="\033[0;1;7;31m"
-C_INFO="\033[0;1;34m"
-C_WARN="\033[0;1;31m"
-C_SUCCESS="\033[0;1;4;32m"
-C_NORMAL="\033[0m"
+#!/bin/bash
+
+export C_ERROR="\033[0;1;7;31m"
+export C_INFO="\033[0;1;34m"
+export C_WARN="\033[0;1;31m"
+export C_SUCCESS="\033[0;1;4;32m"
+export C_BOLD="\033[1m"
+export C_NORMAL="\033[0m"
 
 echo::error() {
   echo -e "${C_ERROR}$@${C_NORMAL}"
